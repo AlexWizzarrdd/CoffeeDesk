4a30e44060ddd2095db40219f8267ffd642fb5a3
simonpavlov <simonpavlov@icloud.com>
2025-11-08 14:35:31 +0500

    Создал модель project

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Project(models.Model):
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="projects",
+        verbose_name="Владелец проекта"
+    )
+    title = models.CharField(max_length=255, verbose_name="Название проекта")
+    description = models.TextField(blank=True, verbose_name="Описание")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата обновления")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Проект"
+        verbose_name_plural = "Проекты"
