4a2a874397efefef5402a58a92a884c52de744c7
simonpavlov <simonpavlov@icloud.com>
2025-11-08 13:41:29 +0500

    добавили поле phone

--- users/models.py
+++ users/models.py
@@ -2,8 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class User(AbstractUser):
-    # можно будет расширить поля позже
-    pass
+    phone = models.CharField(max_length=20, blank=True, null=True)
 
     def __str__(self):
         return self.email or self.username
