128621232830022a4a833d5e70bd2a1336545a5f
simonpavlov <simonpavlov@icloud.com>
2025-11-08 13:29:52 +0500

    feat(auth): добавить вьюхи регистрации и эндпоинт профиля /me

--- users/views.py
+++ users/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .serializers import RegisterSerializer, MeSerializer
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class MeView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        return Response(MeSerializer(request.user).data)
