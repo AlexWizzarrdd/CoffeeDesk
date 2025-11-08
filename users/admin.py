9554c7b0ee04198d06efe5fdd260e5a84ab1a4c8
simonpavlov <simonpavlov@icloud.com>
2025-11-08 13:22:39 +0500

    добавили кастомную модель пользователя

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ("email", "first_name", "last_name", "is_staff")
+    search_fields = ("email", "first_name", "last_name")
+    ordering = ("email",)
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name", "phone")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser")}),
+        ("Important dates", {"fields": ("last_login",)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "password1", "password2", "is_staff", "is_superuser"),
+        }),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
