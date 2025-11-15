573aa7a132a6e3a01210daf994daf1d1e448bf5a
simonpavlov <simonpavlov@icloud.com>
2025-11-15 23:03:25 +0500

    Удалил JWT login

--- CoffeeDesk/urls.py
+++ CoffeeDesk/urls.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
 
-    path("api/auth/", include("users.urls")),
-
-    path("api/auth/login/", TokenObtainPairView.as_view(), name="login"),
     path("api/auth/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/auth/", include("users.urls")),
 ]
