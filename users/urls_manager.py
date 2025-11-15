ea0cc3c8d927c6aa76a66a05eb4816278cad2524
simonpavlov <simonpavlov@icloud.com>
2025-11-15 23:28:52 +0500

    форматирование кода

--- users/urls_manager.py
+++ users/urls_manager.py
@@ -1,9 +1,5 @@
 from django.urls import path
-from .views_manager import (
-    UsersListView,
-    UserApproveView,
-    UserRoleUpdateView,
-)
+from .views_manager import UsersListView, UserApproveView, UserRoleUpdateView
 
 urlpatterns = [
     path("users/", UsersListView.as_view(), name="users_list"),
